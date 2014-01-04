d8d93b038349ff2dc750a93ddf409048ce057a3d
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-04 18:48:02 +0600

    First implementation, I wornder how well it will work

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 'use strict';
 
+var es = require('event-stream');
+var batch = require('gulp-batch');
+var Gaze = require('gaze');
+
+function batched(cb, event) {
+    // Do we need to stream this events next?
+    // through.emit('data', event);
+    cb(event);
+}
+
+function streamed(event) {
+    this.emit('data', event);
+}
+
 module.exports = function (opts, cb) {
-    if (typeof opts === 'function') {
+    if (typeof opts !== 'object') {
         cb = opts;
         opts = {};
     }
 
-    if (typeof cb !== 'function') {
+    if (cb && typeof cb !== 'function') {
         throw new Error('Provided callback is not a function: ' + cb);
     }
 
-    /* if (cb.length < 2) {
-        process.nextTick(cb.bind(null, _batch));
-    } else {
-        holdOn = true;
-        process.nextTick(cb.bind(null, _batch, async));
-    }*/
+    if (cb) {
+        cb = batch(opts, cb);
+    }
 
+    var gaze = new Gaze();
+
+    var through = es.through(function write(file) {
+        gaze.add(file.path);
+    }, function end() {
+        // Neverending story
+    });
+
+    gaze.on('all', cb ? batched.bind(null, cb) : streamed.bind(through));
+
+    return through;
 };
--- test/index.js
+++ test/index.js
@@ -3,11 +3,17 @@
 
 delete require.cache[require.resolve('..')];
 var watch = require('..');
-var assert = require('power-assert');
-var async = require('async');
+var assert = require('assert');
+// var async = require('async');
+var Stream = require('stream').Stream;
 
-describe('glob-batch', function () {
+describe('glob-watch', function () {
     it('should throw, if we provide invalid callback', function () {
         assert.throws(watch.bind(null, 'string'), /Provided callback is not a function/);
+    });
+
+    it('should return stream', function () {
+        assert.ok(watch() instanceof Stream, 'Stream was not returned');
+        assert.ok(watch(function () { }) instanceof Stream, 'Stream was not returned');
     });
 });
