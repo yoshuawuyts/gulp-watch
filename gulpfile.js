f8854629d4f570b3bfc4e7b6b4e5f09cdd3a6c36
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-09 10:39:54 +0600

    Use streaming way to test

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,20 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var watch = require('./index');
+var grep = require('gulp-grep-stream');
 
 // require('longjohn');
 
-gulp.task('watch', function (cb) {
-    gulp.src(['./test/watch.js', 'index.js'], { read: false })
-        .pipe(watch(function (events, cb) {
-            gulp.src(['./test/watch.js'])
-                .pipe(mocha({ timeout: 5000, reporter: 'spec' }))
-                .on('error', function (err) {
-                    console.log(err.toString());
-                    cb();
-                })
-                .on('end', cb);
-        }))
-        .on('end', cb);
+gulp.task('watch', function () {
+    gulp.src(['test/*.js', 'index.js'], { read: false })
+        .pipe(watch())
+        .pipe(grep('**/test/*.js'))
+        .pipe(mocha({ timeout: 5000, reporter: 'spec' }))
+        .on('error', function (err) {
+            if (!/tests? failed/.test(err.stack)) {
+                console.log(err.stack);
+            }
+        });
 });
 
 gulp.task('default', function (cb) {
